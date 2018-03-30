4998c5b05d04abc01192c0ae164346dcc9fa69e3
Mariusz08 <nowygracz01@gmail.com>
2018-03-30 13:34:01 +0200

    First commit

---
+++ q2apro-flag-reasons-lang-default.php
@@ -0,0 +1,21 @@
+<?php
+/*
+	Plugin Name: q2apro Flag Reasons
+*/
+
+return array(
+	'reason' => 'Powod:',
+	'reason_spam' => 'Spam',
+	'reason_quality' => 'Niska wydajnosc',
+	'reason_rude' => 'Insulting',
+	'reason_edit' => 'Edit necessary',
+	'reason_duplicate' => 'Duplikat',
+	'reason_offtopic' => 'Off-topic',
+	'reason_migrate' => 'Migracja',
+	'reason_close' => 'Zamkniêty',
+	'reason_other' => 'Inne',
+	'note' => 'Notatka:',
+	'enter_details' => 'Wpisz notatke',
+	'send' => 'Wyslij',
+	'flaglist_head' => 'Flagi:',
+);
